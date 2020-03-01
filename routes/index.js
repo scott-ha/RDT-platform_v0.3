444e94a7a46743e4eedb3b4bafcce368b2eb6c82
scott-ha <haneul9209@gmail.com>
2020-03-02 01:00:24 +0900

    20200301 first commit

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
   // cookie from req.cookies
   var user_name = req.cookies.MY_USER;
   var session = req.session.logined;
+  console.log(user_name);
   res.render('index', {
     title: 'RealDesignTech',
     session: session,
